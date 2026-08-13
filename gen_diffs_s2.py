a27975d196aab36864f442bf248b80a6696b5c91
Ian Yin <xinghuan.yin@gmail.com>
2026-08-14 05:00:14 +0800

    security: remove embedded R2 credentials

--- gen_diffs_s2.py
+++ gen_diffs_s2.py
@@ -11,9 +11,9 @@ from io import BytesIO
 API_URL       = "http://aiservice.wdabuliu.com:8019/genl_image"
 API_TIMEOUT   = 360
 USER_ID       = 123456
-R2_ACCOUNT_ID = "[removed-r2-account-id]"
-R2_ACCESS_KEY = "[removed-r2-access-key]"
-R2_SECRET_KEY = "[removed-r2-secret-key]"
+R2_ACCOUNT_ID = os.environ["ALTERU_R2_ACCOUNT_ID"]
+R2_ACCESS_KEY = os.environ["ALTERU_R2_ACCESS_KEY_ID"]
+R2_SECRET_KEY = os.environ["ALTERU_R2_SECRET_ACCESS_KEY"]
 R2_BUCKET     = "aigram"
 R2_PUBLIC     = "https://images.aiwaves.tech"
 OUT_DIR       = "src/SpotDiffS2/img/levels"
